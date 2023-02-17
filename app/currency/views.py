7684d0e83b88a99666163a73fc5d68809ed54e0d
volodymyr <dobroslaf777@gmail.com>
2023-02-17 14:43:24 +0200

    change name file add model Rate in models.py add code to views.py

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from currency.models import ContactUs
+from currency.models import Rate
+
+
+def list_rates(request):
+    qs = Rate.objects.all()
+    result = []
+    for rate in qs:
+        result.append(
+            f'id: {rate.id}, buy: {rate.buy}, sell: {rate.sell}, currency: {rate.currency}, source: {rate.source}, created: {rate.created} <br>')
+    return HttpResponse(str(result))
+
+
 def listContactUs(request):
     qs = ContactUs.objects.all()
     result = []
     for rate in qs:
-        result.append(f'id:{rate.id}, email_from:{rate.email_from}, subject:{rate.subject}, massage:{rate.massage} <br>')
+        result.append(
+            f'id:{rate.id}, email_from:{rate.email_from}, subject:{rate.subject}, massage:{rate.massage} <br>')
     return HttpResponse(str(result))
-
 
 # Create your views here.
